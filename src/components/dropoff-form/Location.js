d2a462948074bf73d026f0bd34bd7daa48360dfd
Matt Congdon <mattcongdon95@gmail.com>
2022-07-13 22:25:18 +0100

    select styling

--- src/components/dropoff-form/Location.js
+++ src/components/dropoff-form/Location.js
@@ -7,15 +7,17 @@ export default function Location({ fadeIn, location, handleChange }) {
   return (
     <motion.div className="question__wrap" variants={fadeIn} initial="initial" animate="animate">
       <label htmlFor="location">Choose where you're dropping your bike off from the locations below.</label>
-      <select
-        name="location"
-        id="location"
-        value={location}
-        onChange={handleChange} >
-        <option value="Cycle Exchange Kingston">Cycle Exchange Kingston</option>
-        <option value="Fettle Hyde Park">Fettle Hyde Park</option>
-        <option value="Flamme Rouge Bedford">Flamme Rouge Bedford</option>
-      </select>
+      <div className="select__wrap">
+        <select
+          name="location"
+          id="location"
+          value={location}
+          onChange={handleChange} >
+          <option value="Cycle Exchange Kingston">Cycle Exchange Kingston</option>
+          <option value="Fettle Hyde Park">Fettle Hyde Park</option>
+          <option value="Flamme Rouge Bedford">Flamme Rouge Bedford</option>
+        </select>
+      </div>
     </motion.div>
   )
 }
